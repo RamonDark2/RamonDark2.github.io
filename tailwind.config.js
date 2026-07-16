d19d8b66dcd42121adc72447653840167963fbea
Jalbert Ramon <jalbertramon2@gmail.com>
2026-07-16 17:26:24 -0300

    feat: habilita dark mode, novas fontes e instala lucide-react/framer-motion

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-  extend: {
-    fontFamily: {
-      playfair: ['"Playfair Display"', 'serif'],
+    extend: {
+      fontFamily: {
+        playfair: ['"Playfair Display"', 'serif'],
+        heading: ['"Space Grotesk"', 'sans-serif'],
+        sans: ['"Inter"', 'sans-serif'],
+      },
     },
   },
-},
-
   plugins: [],
 }
